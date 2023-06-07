a768de401dfff5983b4d591b2d4bfbcfa494c4b0
Tornuz <yadavgourav2001@gmail.com>
2023-06-08 00:10:59 +0530

    first commit

--- minesweeper.js
+++ minesweeper.js
@@ -1,13 +1,15 @@
 
 const TILE_STATUSES = {
-    HIDDEN: 'hidden',
-    MINE: 'mine',
-    NUMBER: 'number',
-    MARKED: 'marked'
+    HIDDEN: "hidden",
+    MINE: "mine",
+    NUMBER: "number",
+    MARKED: "marked"
 }
 
 export function createBoard(boardSize, numberOfMines) { 
     const board = []
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+    // console.log(minePositions);
     for (let x = 0; x < boardSize; x++) {
         const row =[]
         for (let y = 0; y < boardSize; y++) {
@@ -16,7 +18,15 @@ export function createBoard(boardSize, numberOfMines) {
             const tile = {
                 element,
                 x,
-                y
+                y,
+                mine:minePositions.some(positionMatch.bind(null, {x, y})),
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                },
+                
             }
             row.push(tile)
             
@@ -26,3 +36,43 @@ export function createBoard(boardSize, numberOfMines) {
     }
     return board
  }
+
+
+export function markTile(tile) {
+    if(tile.status !== TILE_STATUSES.HIDDEN    &&
+       tile.status !== TILE_STATUSES.MARKED
+       ){
+        return
+       }
+
+       if(tile.status === TILE_STATUSES.MARKED) {
+        tile.status = TILE_STATUSES.HIDDEN 
+       } else {
+        tile.status = TILE_STATUSES.MARKED 
+       }
+}
+ 
+
+ function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize)
+        }
+
+        if(!positions.some(p => positionMatch(p, position))) {
+            positions.push(position)
+        }
+    }
+    return positions
+ }
+
+ function positionMatch(a, b) {
+    return a.x === b.x  &&  a.y === b.y
+ }
+
+ function randomNumber(size) { 
+    return Math.floor(Math.random() * size)
+  }
--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-import { createBoard } from "./minesweeper.js";
+import { createBoard, markTile } from "./minesweeper.js";
+
+
+// 1. Populate the board with Tiles/Mines
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 2
+const NUMBER_OF_MINES = 6
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector("[data-mine-count]")
 
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
+
+    // 2. Left click on Tiles
+
+        tile.element.addEventListener("click", () => {})
+
+    // 3. Right click on Tiles
+
+        tile.element.addEventListener("contextmenu", e => {
+            e.preventDefault()
+            markTile(tile)
+        })
+        
     })
 })
 
 boardElement.style.setProperty("--size", BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
+
+
+
+// 1. Populate the board with Tiles/Mines
+// 2. Left click on Tiles
+    // 2.1=> Reveal Tiles
+// 3. Right click on Tiles
+    // 3.1=> Mark Tiles
+// 4. Check for Win/Lose
